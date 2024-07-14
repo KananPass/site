c16070c115ed2a49b04858ba282de9de8c977ab1
manghidev <31641216+manghidev@users.noreply.github.com>
2024-07-14 00:50:42 -0600

    chore: update next.config.mjs, globals.css, and layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body className={ `${inter.className} text-page bg-page` }>
         { children }
       </body>
